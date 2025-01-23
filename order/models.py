e11c096e7207158cdd1222c700f731249d2fedff
Amir Shabani <amishbni@gmail.com>
2025-01-23 20:14:00 +0330

    string representation of Order and OrderItem models

--- order/models.py
+++ order/models.py
@@ -16,11 +16,17 @@ class Order(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
+    def __str__(self):
+        return f"{self.user}: {self.price}"
+
 
 class OrderItem(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
     count = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.order}: {self.count} {self.product}"
 
     def save(self, *args, **kwargs):
         if self.pk is None:
