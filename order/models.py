b7bb93e8262ba9bebc99a660416f7ebabdd89bc4
Amir Shabani <amishbni@gmail.com>
2025-01-23 19:28:22 +0330

    order management models: product, order, and order item

--- order/models.py
+++ order/models.py
@@ -1,3 +1,33 @@
+from core.models import BaseModel
 from django.db import models
 
-# Create your models here.
+
+class Product(BaseModel):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} at {self.price}"
+
+
+class Order(BaseModel):
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+
+class OrderItem(BaseModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    count = models.PositiveIntegerField()
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.order.price += (self.count * self.product.price)
+            self.order.save(update_fields=["price"])
+        else:
+            order_item: OrderItem = OrderItem.objects.get(pk=self.pk)
+            if order_item.count != self.count:
+                changed_count: int = self.count - order_item.count
+                self.order.price += (changed_count * self.product.price)
+                self.order.save(update_fields=["price"])
+        super().save(*args, **kwargs)
+
