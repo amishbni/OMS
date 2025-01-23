5357f9c6861848620c74ce172635f8f9115e1d39
Amir Shabani <amishbni@gmail.com>
2025-01-23 18:42:07 +0330

    make and migrate, collect static files

--- start_services.sh
+++ start_services.sh
@@ -2,6 +2,10 @@
 
 case "$SERVICE" in
    "web")
+      python manage.py makemigrations
+      python manage.py migrate
+      python manage.py collectstatic --no
+
       gunicorn oms.wsgi:application --bind 0.0.0.0:8000
    ;;
 esac
