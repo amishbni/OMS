54e45632fd891431448d41fd45fd341dedd100c6
Amir Shabani <amishbni@gmail.com>
2025-01-24 23:08:44 +0330

    queryset filter based on admin

--- order/views.py
+++ order/views.py
@@ -1,14 +1,20 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
+from user.models import User
 from order.models import Order, Product, OrderItem
 from order.serializers import OrderSerializer, ProductSerializer, OrderItemSerializer
 
 
 class OrderViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
-    queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        user: User = self.request.user
+        if user.is_admin:
+            return Order.objects.all()
+        return Order.objects.filter(user=user)
 
 
 class ProductViewSet(viewsets.ModelViewSet):
